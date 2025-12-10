2bf4f665983f524fc109f42ee4f55e7d0b6b1c72
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:56:00 +0100

    Update dependency versions to latest tags

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
         .library(name: .rfc7617, targets: [.rfc7617]),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.1.0"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-4648", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.6.3"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-4648", from: "0.5.3"),
     ],
     targets: [
         .target(
